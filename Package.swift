ce99883f9177b51e36187a67560e171fb7349f22
aleksey <aleksey@yodelcode.com>
2025-01-08 16:30:18 -0800

    Library is now dynamic.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "EasyDocument",
+			name: "EasyDocument",
+			type: .dynamic,
             targets: ["EasyDocument"]),
     ],
     dependencies: [
